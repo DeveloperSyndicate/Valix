80c58f32f176cd811e767b1a9e0d55b12f0648dd
Sanjay <101442465+imsaba16@users.noreply.github.com>
2026-08-12 18:43:16 +0530

    ci: add automated Dokka multi-module documentation build and GitHub Pages deployment step

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.3.9" apply false
     id("com.android.application") version "9.1.1" apply false
     id("com.android.library") version "9.1.1" apply false
-    id("org.jetbrains.dokka") version "1.9.20" apply false
+    id("org.jetbrains.dokka") version "1.9.20"
     id("com.vanniktech.maven.publish") version "0.29.0" apply false
 }
 
